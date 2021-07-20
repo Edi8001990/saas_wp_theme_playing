7bb6b9fafbb7e8ad966c58d4317250658d274bad
Mateusz Trajdos <mateusz.trajdos@mateuszs-mbp.home>
2021-07-20 23:27:40 +0100

    SAAS custom WP theme playing

---
+++ front-page.php
@@ -0,0 +1,9 @@
+<?php get_header(); ?>
+
+<div class="content">
+
+    <button class="btn btn-primary">Click Here</button>
+
+</div>
+
+<?php get_footer(); ?>
---
+++ webpack.mix.js
@@ -0,0 +1,9 @@
+const mix = require('laravel-mix');
+
+mix.options({processCssUrls: false})
+   .js('src/app.js', 'dist')
+   .sass('src/app.scss', 'dist')
+   .setPublicPath('dist');
+
+
+   
